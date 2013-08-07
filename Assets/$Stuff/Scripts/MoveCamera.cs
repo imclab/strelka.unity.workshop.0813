8d8d7566814ae472c7df8833d8d0340eaf77ff07
Valentin Simonov <v@lent.in>
2013-08-07 14:36:32 +0400

    Quad и MoveCamera

---
+++ Assets/$Stuff/Scripts/MoveCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float Multiplier = 1;
+
+    private void Update()
+    {
+        var player = GameObject.Find("Player");
+        if (player == null) return;
+        transform.localRotation = Quaternion.Euler(0, player.transform.localPosition.x * Multiplier, 0);
+    }
+}
---
+++ Assets/Tmp/Scripts/MakePlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public static class MakePlayer {
+
+    [MenuItem("Component/Make Player")]
+    public static void Do()
+    {
+        if (Selection.activeGameObject == null) return;
+        Debug.Log(Selection.activeGameObject.name);
+
+        if (Selection.activeGameObject.GetComponent<Move>() == null)
+        {
+            var move = Selection.activeGameObject.AddComponent<Move>();
+            move.Speed = 5;
+        }
+        if (Selection.activeGameObject.GetComponent<Fire>() == null)
+        {
+            var fire = Selection.activeGameObject.AddComponent<Fire>();
+            fire.RocketPrefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Ready/Rocket Ready 06.prefab", typeof(GameObject)) as GameObject;
+        }
+        if (Selection.activeGameObject.GetComponent<Player>() == null)
+        {
+            Selection.activeGameObject.AddComponent<Player>();
+        }
+        if (Selection.activeGameObject.GetComponent<BoxCollider>() == null)
+        {
+            Selection.activeGameObject.AddComponent<BoxCollider>();
+        }
+    }
+
+}
